6c89cd3559e6649f7c607bd71194a85400d13e1a
Komaljit Singh <Komaljit.Singh@igglobal.com>
2020-05-14 21:52:40 +0530

    adding project

---
+++ src/main/java/com/study/doctorhelper/DoctorHelperApplication.java
@@ -0,0 +1,13 @@
+package com.study.doctorhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorHelperApplication.class, args);
+	}
+
+}
